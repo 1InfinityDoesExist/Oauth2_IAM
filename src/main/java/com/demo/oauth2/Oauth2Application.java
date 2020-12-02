9f99c07ad413e4ed7072430b0e3188e464eeb8c1
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-12-02 21:48:44 +0530

    refactor

--- src/main/java/com/demo/oauth2/Oauth2Application.java
+++ src/main/java/com/demo/oauth2/Oauth2Application.java
@@ -3,17 +3,22 @@ package com.demo.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.SpringVersion;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import lombok.extern.slf4j.Slf4j;
 
 @EnableAuthorizationServer
 @EnableResourceServer
 @SpringBootApplication
-@ComponentScan({"com.fb.demo", "com.demo.oauth2"})
+@Slf4j
+@ComponentScan({ "com.fb.demo", "com.demo.oauth2" })
 public class Oauth2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Oauth2Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+		log.info(":::::Spring Version : {}", SpringVersion.getVersion());
+	}
 
 }
